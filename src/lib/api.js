057082d3078827bdd984b17e08cc841cccfdd966
Chris Bongers <chrisbongers@gmail.com>
2021-09-25 16:25:21 +0200

    Initial commit ❤️

---
+++ src/lib/api.js
@@ -0,0 +1,75 @@
+import dotenv from 'dotenv';
+dotenv.config();
+const API_URL = process.env.WP_URL;
+
+async function fetchAPI(query, { variables } = {}) {
+  const headers = { 'Content-Type': 'application/json' };
+  const res = await fetch(API_URL, {
+    method: 'POST',
+    headers,
+    body: JSON.stringify({ query, variables }),
+  });
+
+  const json = await res.json();
+  if (json.errors) {
+    console.log(json.errors);
+    throw new Error('Failed to fetch API');
+  }
+
+  return json.data;
+}
+
+export async function getAllPagesWithSlugs() {
+  const data = await fetchAPI(`
+  {
+    pages(first: 10000) {
+      edges {
+        node {
+          slug
+        }
+      }
+    }
+  }
+  `);
+  return data?.pages;
+}
+
+export async function getPageBySlug(slug) {
+  const data = await fetchAPI(`
+  {
+    page(id: "${slug}", idType: URI) {
+      title
+      content
+    }
+  }
+  `);
+  return data?.page;
+}
+
+export async function getPrimaryMenu() {
+  const data = await fetchAPI(`
+  {
+    menus(where: {location: PRIMARY}) {
+      nodes {
+        menuItems {
+          edges {
+            node {
+              path
+              label
+              connectedNode {
+                node {
+                  ... on Page {
+                    isPostsPage
+                    slug
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+  `);
+  return data?.menus?.nodes[0];
+}
---
+++ tailwind.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  mode: 'jit',
+  purge: ['./public/**/*.html', './src/**/*.{astro,js,jsx,svelte,ts,tsx,vue}'],
+  plugins: [require('@tailwindcss/typography')],
+};
